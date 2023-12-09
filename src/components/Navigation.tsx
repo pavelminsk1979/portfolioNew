06f40ec753528092530ca00e9748bf298897b819
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-12-09 15:26:14 +0300

    commit switch dark theme

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,17 @@
 import st from './Navigation.module.scss'
 
+import { SwitchComponent } from './switchComponent/SwitchComponent'
+
 export const Navigation = () => {
-  return <div className={st.main}>Кратко</div>
+  const handlerOnChackedChange = (checked: boolean) => {
+    console.log(checked)
+    document.body.classList.toggle('dark-mode', checked)
+  }
+
+  return (
+    <div>
+      <div className={st.main}>Кратко</div>
+      <SwitchComponent handlerOnChackedChange={handlerOnChackedChange} />
+    </div>
+  )
 }
