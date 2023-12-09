5e9ded61cd00052a5f10aeb4e1f56715ebf8481f
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-12-09 14:04:34 +0300

    commit settongs

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { RouterProvider } from 'react-router-dom'
 
 import { createRoot } from 'react-dom/client'
 
+import './styles/index.scss'
+import '@fontsource/roboto-condensed/400.css'
+import '@fontsource/roboto-condensed/700.css'
+
 import { router } from './router/router'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
