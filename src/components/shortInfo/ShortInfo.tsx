c8778c04da4c7578ea7c81283877c466e5bb4fe7
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-12-13 02:04:26 +0300

    finish short info

--- src/components/shortInfo/ShortInfo.tsx
+++ src/components/shortInfo/ShortInfo.tsx
@@ -20,12 +20,27 @@ export const ShortInfo = () => {
       <div className={st.header} onClick={handleOnClickNavigate}>
         <span onClick={handleOnClickNavigate}>вернуться на главную</span>
       </div>
+
       <div className={st.blockContent}>
         <div className={st.frame}>
           <div className={st.upperAngles}>
             <div className={st.upLt} />
             <div className={st.upRt} />
           </div>
+
+          <div className={st.blockText}>
+            <div className={st.text}>
+              <p>
+                Павел Потапов. Фронденд разработчик. Мой стек технологий включает: React,
+                Redux,Redux Toolkit, RTK Query, JavaScript,TypeScript,GIT , SCSS. Разрабатываю
+                компоненты, покрываю код тестами.Имею опыт работы с различными UI-библиотеками.Опыт
+                работы в комерческой разработке 1 год 6 месяцев. <br /> pavvel.potapov@gmail.com
+                <br />
+                https://t.me/pavvelpotapov
+              </p>
+            </div>
+          </div>
+
           <div className={st.lowerAngles}>
             <div className={st.lowLt} />
             <div className={st.lowRt} />
