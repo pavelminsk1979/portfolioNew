95e09bcda16b282ccba7a5e6701d1ff229b8af9a
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-12-12 23:53:08 +0300

    add fonFoto

--- src/components/shortInfo/ShortInfo.tsx
+++ src/components/shortInfo/ShortInfo.tsx
@@ -2,16 +2,28 @@ import { useNavigate } from 'react-router-dom'
 
 import st from './ShortInfo.module.scss'
 
+import { SwitchComponent } from '../switchComponent/SwitchComponent'
+
 export const ShortInfo = () => {
   const navigate = useNavigate()
+
   const handleOnClickNavigate = () => {
     navigate('/')
   }
 
+  const handlerOnChackedChange = (checked: boolean) => {
+    document.body.classList.toggle('dark-mode', checked)
+  }
+
   return (
     <div className={st.main}>
-      <div onClick={handleOnClickNavigate}>Вернутся на главную</div>
-      <div>КРАТКО ОБО МНЕ</div>
+      <div className={st.header} onClick={handleOnClickNavigate}>
+        <span onClick={handleOnClickNavigate}>вернуться на главную</span>
+      </div>
+
+      <div className={st.switchDarkTheme}>
+        <SwitchComponent handlerOnChackedChange={handlerOnChackedChange} />
+      </div>
     </div>
   )
 }
