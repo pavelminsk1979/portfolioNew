e2f1b817326c8b9ff21fe58c9ed65710f0906a61
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-12-17 02:16:27 +0300

    refactor...

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,6 @@
 import st from './Main.module.scss'
 
 import myFotoImage from '../../assets/image/myFoto.jpg'
-import { Scils } from '../scils/Scils'
 
 export const Main = () => {
   const myFoto = {
@@ -9,15 +8,12 @@ export const Main = () => {
   }
 
   return (
-    <div className={st.main}>
-      <div className={st.blockFotoAndInfo}>
-        <p className={st.text}>
-          Меня зовут Павел. Я фронтенд-разработчик с опытом коммерческой разработки 1.5 года.
-          Специализируюсь на создании пользовательского интерфейса SPA-приложений.
-        </p>
-        <div className={st.foto} style={myFoto}></div>
-      </div>
-      <Scils />
+    <div className={st.blockFotoAndInfo}>
+      <p className={st.text}>
+        Меня зовут Павел. Я фронтенд-разработчик с опытом коммерческой разработки 1.5 года.
+        Специализируюсь на создании пользовательского интерфейса SPA-приложений.
+      </p>
+      <div className={st.foto} style={myFoto}></div>
     </div>
   )
 }
--- src/page/content/Content.tsx
+++ src/page/content/Content.tsx
@@ -2,12 +2,14 @@ import st from './Content.module.scss'
 
 import { Navigation } from '../../components/Navigation'
 import { Main } from '../../components/main/Main'
+import { Scils } from '../../components/scils/Scils'
 
 export const Content = () => {
   return (
     <div className={st.main}>
       <Navigation />
       <Main />
+      <Scils />
 
       {/* <div className={st.main}>блок на всю страницу</div>*/}
     </div>
