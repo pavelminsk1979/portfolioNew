d711aa4ff505d95564940f50ba716a2ade4a7f02
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-12-24 15:50:11 +0300

    refactor.........

---
+++ src/constants/constantDataProjects.ts
@@ -0,0 +1,36 @@
+import imgSamuraiWai from '../assets/image/fotoSamuraiWay.jpg'
+import imgLearnDecks1 from '../assets/image/learnDecks1.jpg'
+import imgLearnDecks2 from '../assets/image/learnDecks2.jpg'
+import imgLearnDecks3 from '../assets/image/learnDecks3.jpg'
+import imgLearnDecks4 from '../assets/image/learnDecks4.jpg'
+import imgLearnDecks5 from '../assets/image/learnDecks5.jpg'
+import imgLearnDeckMains from '../assets/image/learnDecksMain.jpg'
+import imgMySocial from '../assets/image/mySamuraiWai.jpg'
+import imgSamuraiWaiLogin from '../assets/image/mySamuraiWaiLogin.jpg'
+import imgSocial from '../assets/image/social.png'
+import imgTask1 from '../assets/image/task1jpg.jpg'
+import imgTask2 from '../assets/image/task2.jpg'
+import imgTask3 from '../assets/image/task3.jpg'
+import imgTask4 from '../assets/image/task4jpg.jpg'
+import imgTaskMain from '../assets/image/taskMain.jpg'
+
+export const DATA_PROJECTS = [
+  { id: 0, imgsAloneProject: [imgSocial, imgSamuraiWai, imgMySocial, imgSamuraiWaiLogin] },
+  { id: 1, imgsAloneProject: [imgTaskMain, imgTask1, imgTask2, imgTask3, imgTask4] },
+  {
+    id: 2,
+    imgsAloneProject: [
+      imgLearnDeckMains,
+      imgLearnDecks1,
+      imgLearnDecks2,
+      imgLearnDecks3,
+      imgLearnDecks4,
+      imgLearnDecks5,
+    ],
+  },
+]
+
+export type DataProject = {
+  id: number
+  imgsAloneProject: string[]
+}
