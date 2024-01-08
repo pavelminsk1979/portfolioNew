9a1e3b0db893b03ece9f9a072305a106edcbaf9a
pavelminsk1979 <pavelminsk1979@mail.ru>
2024-01-08 19:13:45 +0300

    add imgPhoneTelegramMail

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,13 +1,22 @@
 import st from './Contact.module.scss'
 import { FormSendMessage } from './formSendMessage/FormSendMessage.tsx'
+import imgPhone from '../../assets/image/phone.png'
+import imgTelegram from '../../assets/image/telegr.jpg'
+import imgMail from '../../assets/image/mail.png'
 
 export const Contact = () => {
   return (
     <div className={st.main}>
       <div className={st.blocLink}>
-        <div className={st.minBlockLink}>1</div>
-        <div className={st.minBlockLink}>2</div>
-        <div className={st.minBlockLink}>3</div>
+        <div className={st.minBlockLink}>
+          <img alt={'фото проэкта '} className={st.imgPhoneTelegramMail} src={imgPhone} />
+        </div>
+        <div className={st.minBlockLink}>
+          <img alt={'фото проэкта '} className={st.imgPhoneTelegramMail} src={imgTelegram} />
+        </div>
+        <div className={st.minBlockLink}>
+          <img alt={'фото проэкта '} className={st.imgPhoneTelegramMail} src={imgMail} />
+        </div>
       </div>
 
       <FormSendMessage />
